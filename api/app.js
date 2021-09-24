8278c4811875195271b59fbeea8bc91886fc1cf8
MatheusTrinca <matheusfelipetrinca@gmail.com>
2021-09-24 20:59:21 -0300

    finalizada api

--- api/app.js
+++ api/app.js
@@ -5,20 +5,37 @@ dotenv.config();
 import './src/database';
 
 import express from 'express';
+import multer from 'multer';
 import userRoutes from './src/routes/userRoutes';
 import authRoutes from './src/routes/authRoutes';
 import postRoutes from './src/routes/postRoutes';
 import categoryRoutes from './src/routes/categoryRoutes';
+import isLoggedIn from './src/middlewares/isLoggedIn';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/users', userRoutes);
-app.use('/auth', authRoutes);
-app.use('/posts', postRoutes);
-app.use('/categories', categoryRoutes);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'src/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, 'hello.jpg');
+  },
+});
+
+const upload = multer({ storage });
+
+app.use('/api/upload', isLoggedIn, upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/categories', categoryRoutes);
 
 export default app;
 
